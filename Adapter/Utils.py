56cdc21022c9f2057a13fe12a6f7a710f73878a5
Daniel Z <daniel.zimmermann2@kit.edu>
2018-05-18 09:54:51 +0200

    Kommentar hinzugefügt

--- Adapter/Utils.py
+++ Adapter/Utils.py
@@ -6,6 +6,7 @@ def rotate(point, center, angle_deg):
     # degree -> rad
     angle_rad = math.radians(angle_deg)
 
+    # Execute Transformation
     x = math.cos(angle_rad) * (point[0] - center[0]) - math.sin(angle_rad) * (point[1] - center[1]) + center[0]
     y = math.sin(angle_rad) * (point[0] - center[0]) + math.cos(angle_rad) * (point[1] - center[1]) + center[1]
     new_point = [x,y]
